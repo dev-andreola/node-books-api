7f2e7634b0d3cf5b026e7ae247a819163cc446b6
vtAndreola <dev.andreola@gmail.com>
2024-07-08 17:39:21 -0300

    feat: create unique book route

--- src/app.js
+++ src/app.js
@@ -18,6 +18,12 @@ const books = [
   },
 ];
 
+function getBooksByID(id) {
+  return books.findIndex((book) => {
+    return book.id === Number(id);
+  });
+}
+
 app.get("/", (req, res) => {
   res.status(200).send("Mensagem que trafega na rota raiz");
 });
@@ -29,6 +35,15 @@ app.get("/books", (req, res) => {
 app.post("/books", (req, res) => {
   books.push(req.body);
   res.status(200).send("Livro adicionado com sucesso!");
+});
+
+app.get("/book/:id", (req, res) => {
+  const i = getBooksByID(req.params.id);
+  if (i === -1) {
+    res.status(404).send("Livro não encontrado!");
+  } else {
+    res.status(200).json(books[i]);
+  }
 });
 
 export default app;
