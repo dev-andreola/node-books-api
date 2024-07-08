ac79b61fe968ca3dd22f683fc5e03ba45bb1c9bb
vtAndreola <dev.andreola@gmail.com>
2024-07-08 17:26:22 -0300

    feat: create books route

--- src/app.js
+++ src/app.js
@@ -2,8 +2,27 @@ import express from "express";
 
 const app = express();
 
+const books = [
+  {
+    id: 1,
+    title: "Taxi Driver",
+  },
+  {
+    id: 2,
+    title: "The Godfather",
+  },
+  {
+    id: 3,
+    title: "Iron Man",
+  },
+];
+
 app.get("/", (req, res) => {
   res.status(200).send("Mensagem que trafega na rota raiz");
+});
+
+app.get("/books", (req, res) => {
+  res.status(200).json(books);
 });
 
 export default app;
