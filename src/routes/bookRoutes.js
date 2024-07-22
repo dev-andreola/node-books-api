eaa14322cffd6eee2cef7c9cd2568a492343c555
vtAndreola <dev.andreola@gmail.com>
2024-07-22 19:14:16 -0300

    fix: routes dinamic url

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -4,9 +4,9 @@ import BookController from "../controllers/bookController.js";
 const routes = express.Router();
 
 routes.get("/books", BookController.getBooks);
-routes.get("/book:id", BookController.getBookById);
+routes.get("/book/:id", BookController.getBookById);
 routes.post("/books", BookController.postBook);
-routes.put("/book:id", BookController.putBookById);
-routes.delete("/book:id", BookController.deleteBookById);
+routes.put("/book/:id", BookController.putBookById);
+routes.delete("/book/:id", BookController.deleteBookById);
 
 export default routes;
