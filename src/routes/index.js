2df64d6efc623a16739edf370ceb0d6afbf49f7f
vtAndreola <dev.andreola@gmail.com>
2024-07-24 21:32:42 -0300

    feat: add eslint

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,10 +3,12 @@ import bookRoutes from "../routes/bookRoutes.js";
 import authorRoutes from "../routes/authorRoutes.js";
 
 const routes = (app) => {
+  // criando a rota raíz
   app.route("/").get((req, res) => {
     res.status(200).send({ title: "Curso de Node" });
   });
 
+  // usando as rotas criadas
   app.use(express.json(), bookRoutes, authorRoutes);
 };
 
