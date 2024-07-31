e3a28853674128c470c14dae75ee4bb9d506803d
vtAndreola <dev.andreola@gmail.com>
2024-07-31 17:47:56 -0300

    feat: create get books route ordenation

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -5,15 +5,21 @@ import InvalidReq from "../errors/InvalidReq.js";
 class BookController {
   static getBooks = async (req, res, next) => {
     try {
-      let { limit = 5, page = 1 } = req.query;
+      let { limit = 5, page = 1, ordenation = "title:-1" } = req.query;
+
+      let [orderBy, direction] = (
+        typeof ordenation === "string" ? ordenation : "title:-1"
+      ).split(":");
 
       limit = parseInt(limit);
       page = parseInt(page);
+      direction = parseInt(direction);
 
       if (limit > 0 && page > 0) {
         // essa variável recebe todos os objetos de modelo livro encontrados no banco de dados
         const booksFound = await book
           .find()
+          .sort({ [orderBy]: direction })
           .skip((page - 1) * limit)
           .limit(limit)
           .populate("author")
